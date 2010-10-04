715c7aa77ffe1309e27672a5a53dcef268246485
Andrew Schleifer <me@andrewschleifer.name>
2010-10-03 20:34:34 -0500

    Add "About Shù" to context menu

--- BApplicationDelegate.h
+++ BApplicationDelegate.h
@@ -1,13 +1,19 @@
+@class WebView;
 
 @interface BApplicationDelegate : NSObject <NSApplicationDelegate> {
+	WebView *credits;
+	NSWindow *about;
     NSWindow *window;
 	NSString *display;
 	id mail;
 }
 
+@property (assign) IBOutlet WebView *credits;
+@property (assign) IBOutlet NSWindow *about;
 @property (assign) IBOutlet NSWindow *window;
 @property (copy) NSString *display;
 
+-(IBAction)showAboutPanel:(id)sender;
 -(void)update:(NSTimer *)timer;
 
 @end
--- BApplicationDelegate.m
+++ BApplicationDelegate.m
@@ -1,9 +1,12 @@
 
 #import "BApplicationDelegate.h"
 #import "Mail.h"
+#import "WebKit/WebKit.h"
 
 @implementation BApplicationDelegate
 
+@synthesize credits;
+@synthesize about;
 @synthesize window;
 @synthesize display;
 
@@ -16,6 +19,19 @@
 								   selector:@selector(update:)
 								   userInfo:nil
 									repeats:YES];
+	NSString *creditsPath = [[NSBundle mainBundle] pathForResource:@"Credits"
+															ofType:@"html"];
+	NSData *creditsData = [NSData dataWithContentsOfFile:creditsPath];
+	[[credits mainFrame] loadData:creditsData
+						 MIMEType:@"text/html"
+				 textEncodingName:@"UTF-8"
+						  baseURL:nil];
+}
+
+-(IBAction)showAboutPanel:(id)sender
+{
+	[about center];
+	[about makeKeyAndOrderFront:self];
 }
 
 -(void)update:(NSTimer *)timer
